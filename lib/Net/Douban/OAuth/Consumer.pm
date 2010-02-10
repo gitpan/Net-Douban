e6f6b715f9dd12348b6cc41647eb17dcb7729944
woosley <redicaps@cpan.org>
2010-02-10 09:31:49 +0000

    Import of REDICAPS/Net-Douban-1.06 from CPAN.
    gitpan-cpan-distribution: Net-Douban
    gitpan-cpan-version:      1.06
    gitpan-cpan-path:         REDICAPS/Net-Douban-1.06.tar.gz
    gitpan-cpan-author:       REDICAPS
    gitpan-cpan-maturity:     released

--- lib/Net/Douban/OAuth/Consumer.pm
+++ lib/Net/Douban/OAuth/Consumer.pm
@@ -223,11 +223,11 @@ __END__
 
 =head1 NAME
 
-Net::Douban::OAuth::Consumer
+Net::Douban::OAuth::Consumer - Base consumer object for Net::Douban::OAuth
 
 =head1 VERSION
 
-version 1.04
+version 1.06
 
 =head1 SYNOPSIS
     
